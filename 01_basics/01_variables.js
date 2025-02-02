aecdaf19f3f852d42edf25a8939e9dc22437fa86
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-02-02 16:36:18 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const account_id=144553   // const means the value cant be changed
+// account_id=10   (not allowed to change)
+let account_email="tanishgarg@gmail.com"
+var password="12345678"
+account_city="Patiala"   // dont use this method
+let account_state;
+
+// there are two method to declare a variable
+// USE LET AND CONST
+// DONT USE VAR -> due to issue in block and functional scope
+
+console.log(account_id) 
+console.table([account_id,account_email,password,account_city,account_state])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  causes error cos we are using nodejs , not browser
+
+let name="tanish"
+let age=21
+let loggedin=false
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null  ->standalone value 
+// undefined -> 
+// symbol -> to find uniqueness
+
+// object 
+
+console.log(typeof name )
+console.log(typeof null )
