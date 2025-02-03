ca55c3c26aea5d507d370d270a93d2acc9a0782e
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-02-03 21:04:52 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,39 @@
+let score1=100
+let score2="abc"
+//console.log(typeof score1)
+//console.log(typeof(score2))
+
+let vaule_in_no=Number(score2)
+// console.log(typeof vaule_in_no)
+// console.log(vaule_in_no)  // it does print Nan cos "abc" is not a number
+
+// "33" -> 33
+// "33abc" -> NaN
+// true->1   false->0
+// 1->true   0->false
+// "" -> false
+// "tanish" -> true
+
+// ***********************************************************************
+
+let value=3
+let neg_value=-value
+// console.log(neg_value)
+// console.log(2**3)  to print 8
+
+let str1="tanish "
+let str2="garg"
+//console.log(str1+str2) /. concatination of 2 strings
+
+console.log("1"+2+2)
+console.log("1"+"2"+2)
+console.log(1+2+"2"+"2")
+// 122
+// 122
+// 322
+
+console.log(+true) // not a good option
+let x=10
+console.log(x++)
+let y=10
+console.log(++y)
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,7 @@
+console.log(2>1)
+console.log(2==1)
+console.log(2!=1)
+
+//  console.log("2"!=1) this is a wrong method
+// Dont make comparisons between two diff data types
+
